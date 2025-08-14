22f1278762e3ad554cc0363757d418cce242ba91
OGUNKOYA GBENGA <davidogunkoya80@gmail.com>
2025-08-14 00:34:54 +0000

    this isnt a single page what is wrong with you. create another page the

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Hero } from '@/components/hero';
-import { About } from '@/components/about';
-import { Advantages } from '@/components/advantages';
 import { Services } from '@/components/services';
-import  CoreValues from '@/components/core-values';
 import  Portfolio from '@/components/portfolio';
 import { Faq } from '@/components/faq';
 import { HvacEstimator } from '@/components/hvac-estimator';
@@ -17,10 +14,7 @@ export default function Page() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <About />
-        <Advantages />
         <Services />
-        <CoreValues />
         <Portfolio />
         <Faq />
         <HvacEstimator />
